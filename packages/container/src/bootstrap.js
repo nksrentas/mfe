6bccba55da52b051d97c1cf549dd1bf819d750df
nrentas <nrentas@altair.com>
2024-09-08 10:28:11 +0300

    Refactor bootstrap.js: remove redundant code for mounting App component

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,16 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
-const mount = ({ el }) => {
-  return ReactDom.render(<App />, el);
-};
-
-if (process.env.NODE_ENV === 'development') {
-  const el = document.querySelector('#root');
-
-  if (el) {
-    mount({ el });
-  }
-}
-
-export { mount };
+ReactDom.render(<App />, document.querySelector('#root'));
